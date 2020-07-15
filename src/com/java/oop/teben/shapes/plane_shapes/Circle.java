1b8c33d76fa942f1cf9bca58219ae07382d03a44
Artur Teben <teben.artur99@gmail.com>
2020-07-15 10:57:59 +0300

    Create and implement class Circle
    Add field radius, create constructor; overload methods: getArea, getPerimeter, toString

--- src/com/java/oop/teben/shapes/plane_shapes/Circle.java
+++ src/com/java/oop/teben/shapes/plane_shapes/Circle.java
@@ -1,13 +1,33 @@
-package com.java.oop.teben.Shapes.PlaneShapes;
+package com.java.oop.teben.shapes.plane_shapes;
+
+import com.java.oop.teben.points.Vertices2D;
+
+import java.util.List;
 
 public class Circle extends PlaneShape{
+    private final double radius;
+
+    public Circle(Vertices2D vertex, String nameOfShape, double radius) {
+        super(List.of(vertex), "Circle");
+        this.radius = radius;
+    }
+
     @Override
     public double getArea() {
-        return 0;
+        return Math.PI * Math.pow(radius, 2);
     }
 
     @Override
     public double getPerimeter() {
-        return 0;
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "Radius = " + radius +
+                ", Area = " + this.getArea() +
+                ", Length = " + this.getPerimeter() +
+                '}';
     }
 }
