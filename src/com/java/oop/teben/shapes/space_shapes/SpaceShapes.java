17bab173f6233dcf63de4142c61c609016000815
Artur Teben <teben.artur99@gmail.com>
2020-07-15 11:02:50 +0300

    Add constructor

--- src/com/java/oop/teben/shapes/space_shapes/SpaceShapes.java
+++ src/com/java/oop/teben/shapes/space_shapes/SpaceShapes.java
@@ -1,14 +1,13 @@
-package com.java.oop.teben.Shapes.SpaceShapes;
+package com.java.oop.teben.shapes.space_shapes;
 
-import com.java.oop.teben.Interfaces.AreaMeasurable;
-import com.java.oop.teben.Interfaces.VolumeMeasurable;
-import com.java.oop.teben.Points.Vertices_3D;
-import com.java.oop.teben.Shapes.PlaneShapes.PlaneShape;
+import com.java.oop.teben.interfaces.AreaMeasurable;
+import com.java.oop.teben.interfaces.VolumeMeasurable;
+import com.java.oop.teben.shapes.plane_shapes.PlaneShape;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class SpaceShapes extends PlaneShape implements AreaMeasurable, VolumeMeasurable {
-    private List<Vertices_3D> listOfVertics3D = new ArrayList<>();
-
+    public SpaceShapes(List vertices, String nameOfShape) {
+        super(vertices, nameOfShape);
+    }
 }
