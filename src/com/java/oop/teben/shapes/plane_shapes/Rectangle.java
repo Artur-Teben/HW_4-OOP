809d453e155f879be6d4ac653429a5a5c413835c
Artur Teben <teben.artur99@gmail.com>
2020-07-15 10:55:43 +0300

    Create and implement class Recntangle
    Add fields width and height, create constructor, methods getArea, getPerimeter and overload toString

--- src/com/java/oop/teben/shapes/plane_shapes/Rectangle.java
+++ src/com/java/oop/teben/shapes/plane_shapes/Rectangle.java
@@ -1,13 +1,37 @@
-package com.java.oop.teben.Shapes.PlaneShapes;
+package com.java.oop.teben.shapes.plane_shapes;
 
-public class Rectangle extends PlaneShape{
+import com.java.oop.teben.points.Vertices2D;
+
+import java.util.List;
+
+public class Rectangle extends PlaneShape {
+
+    private final double width;
+    private final double height;
+
+    public Rectangle(Vertices2D A, String nameOfShape, double width, double height) {
+        super(List.of(A), "Rectangle");
+        this.width = width;
+        this.height = height;
+    }
+
     @Override
     public double getArea() {
-        return 0;
+        return width * height;
     }
 
     @Override
     public double getPerimeter() {
-        return 0;
+        return (width + height) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "Width = " + width +
+                ", Height = " + height +
+                ", Area = " + this.getArea() +
+                ", Perimeter = " + this.getPerimeter() +
+                '}';
     }
 }
