f3ec6dcbdf55fc7be3981b45413ba844c4a190cf
Artur Teben <teben.artur99@gmail.com>
2020-07-20 13:46:28 +0300

    Implement getDistance in Vertices2D

--- src/com/java/oop/teben/points/Vertices2D.java
+++ src/com/java/oop/teben/points/Vertices2D.java
@@ -3,13 +3,24 @@ package com.java.oop.teben.points;
 
 public class Vertices2D extends Vertex {
     private final float y;
+    private final float x;
 
     public Vertices2D(float x, float y) {
-        super(x);
+        this.x = x;
         this.y = y;
     }
 
     public float getY() {
         return y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    // AB = ((xB - xA)^2 + (yB - yA)^2)^(1/2)
+    public float getDistance(Vertices2D pointA, Vertices2D pointB) {
+        return (float) (Math.sqrt(Math.pow((pointA.x - pointB.x), 2) +
+                Math.pow((pointA.y - pointB.y), 2)));
     }
 }
