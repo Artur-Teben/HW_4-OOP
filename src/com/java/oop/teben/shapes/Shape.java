3d3ef6861489345883de1177c09aa3aadcfec7df
Artur Teben <teben.artur99@gmail.com>
2020-07-20 13:47:20 +0300

    Rename name of class Shape

--- src/com/java/oop/teben/shapes/Shape.java
+++ src/com/java/oop/teben/shapes/Shape.java
@@ -2,16 +2,24 @@ package com.java.oop.teben.shapes;
 
 import java.util.List;
 
-public abstract class Shapes<Vertex> {
+public abstract class Shape<Vertex> {
     protected final List<Vertex> listOfVertices;
     private final String nameOfShape;
 
-    public Shapes(List<Vertex> listOfVertices, String nameOfShape) {
+    public Shape(List<Vertex> listOfVertices, String nameOfShape) {
         this.listOfVertices = listOfVertices;
         this.nameOfShape = nameOfShape;
     }
 
     public String getNameOfShape() {
         return nameOfShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "listOfVertices=" + listOfVertices +
+                ", nameOfShape='" + nameOfShape + '\'' +
+                '}';
     }
 }
