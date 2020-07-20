7d9cf771f720230eaf1d3eab89e93e8bc4f18abd
Artur Teben <teben.artur99@gmail.com>
2020-07-20 13:46:41 +0300

    Implement getDistance in Vertices3D

--- src/com/java/oop/teben/points/Vertices3D.java
+++ src/com/java/oop/teben/points/Vertices3D.java
@@ -20,4 +20,11 @@ public class Vertices3D extends Vertices2D {
                 ", z=" + z +
                 '}';
     }
+
+    // AB = ((xB - xA)^2 + (yB - yA)^2 + (zB - zA)^2)^(1/2)
+    public float getDistance(Vertices3D pointA, Vertices3D pointB) {
+        return (float) (Math.sqrt(Math.pow((pointA.getX() - pointB.getX()), 2) +
+                Math.pow((pointA.getY() - pointB.getY()), 2) +
+                Math.pow((pointA.getZ() - pointB.getZ()), 2)));
+    }
 }
