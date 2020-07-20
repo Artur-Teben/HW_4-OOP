e2652b251638231559e8362db82fec75f1e24563
Artur Teben <teben.artur99@gmail.com>
2020-07-20 13:45:18 +0300

    Delete content of class

--- src/com/java/oop/teben/points/Vertex.java
+++ src/com/java/oop/teben/points/Vertex.java
@@ -1,13 +1,4 @@
 package com.java.oop.teben.points;
 
 public abstract class Vertex {
-    private final float x;
-
-    public Vertex(float x) {
-        this.x = x;
-    }
-
-    public float getX() {
-        return x;
-    }
 }
